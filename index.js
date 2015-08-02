06db73caaf62ff54ae9d2f3142a72c2af0c35292
Zaki Saadeh <zaki.saadeh@gmail.com>
2015-08-01 22:52:00 -0500

    1. added setting to allow for mapping routes to tables/ business models (using regex) 2. added underscore js to project

--- index.js
+++ index.js
@@ -7,16 +7,36 @@ var HappyCamper = require('./lib');
 var server = new Hapi.Server();
 server.connection({ port: 3000 });
 
-
-server.route({
+server.route([
+    {
     method: 'GET',
     path: '/',
     handler: function (request, reply) {
-        reply('Hello, world!');
+            reply('Hello, world!');
+        }
+    },
+    {
+    method: 'GET',
+    path: '/hello',
+    handler: function (request, reply) {
+            reply('this goes to table dictator!');
+        }
+    },
+    {
+    method: 'GET',
+    path: '/test',
+    handler: function (request, reply) {
+            reply('this goes to table test!');
+        }
     }
-});
+]);
 
 
+var routesMap = [
+    { pattern: "/hel+o", name: "dictator"},
+    { pattern: "/te.*" , name: "test"},
+    { pattern:".*" , name: "main"}
+];
 
 server.register({
     register: Good,
@@ -27,7 +47,9 @@ server.register({
             reporter: HappyCamper,
             events: {
                 response: '*',
-                // log: '*'
+            },
+            config:{
+                routesMap: routesMap
             }
         }]
     }
